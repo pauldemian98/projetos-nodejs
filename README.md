941aeffadb926c0ca1df959fe0a479e12fe6b86d
Demian Senna <paulosilvasa2020@outlook.com>
2024-11-06 15:45:46 -0300

    Concluido o portal de noticias dinamico usando o banco de dados Mongo DB Atlas

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Fique a vontade caso deseje usar como material de Consulta, abaixo coloco como o
 - standards: Contém funções mais complexas, utilizando módulos personalizados também;
 - taskList: meu primeiro projeto em Node.js que trata-se de uma aplicação para adicionar e remover tarefas simples sem uso de banco de dados;
 - newsWebStatic: segundo projeto em Node.js trata-se de um portal de noticias estatico sem uso de banco de dados (imagens geradas por ia);
+- newsWebDynamic: terceiro projeto em Node.js, usando EJS para server side rendering e Mongoose para trabalhar com o banco de dados, o objetivo é criar um portal de noticias agora dinâmico usando banco de Dados (não relacional) - Mongo DB Atlas
 
 Desenvolvido por: Paulo Lucas Silva Sá
 
---
+++ newsWebDynamic/index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Posts = require("./posts.js");
+//senha: JTsJPqUUGuMe4IoY
+mongoose
+  .connect(
+    "SubstituirPelolinkDoProprioMongoDBAtlas",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(function () {
+    console.log("Conectado com Sucesso");
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
+
+app.engine("html", require("ejs").renderFile); //utiliza ejs para renderizar o html
+app.set("view engine", "html"); //view engine setada pra ser html
+app.use("/public", express.static(path.join(__dirname, "public"))); //tudo que eh estatico fica em public
+app.set("views", path.join(__dirname, "./pages")); //pasta onde esta as views
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  console.log(req.query);
+
+  if (req.query.busca == null) {
+    Posts.find({})
+      .sort({ _id: -1 })
+      .exec(function (err, posts) {
+        posts = posts.map(function (val) {
+          return {
+            titulo: val.titulo,
+            conteudo: val.conteudo,
+            descricaoResumida: val.conteudo.substring(0, 200),
+            descricaoUltNoticias: val.conteudo.substring(0, 300),
+            imagem: val.imagem,
+            slug: val.slug,
+            categoria: val.categoria,
+          };
+        });
+
+        Posts.find({})
+          .sort({ views: -1 })
+          .limit(3)
+          .exec(function (err, postsTop) {
+            postsTop = postsTop.map(function (val) {
+              return {
+                titulo: val.titulo,
+                conteudo: val.conteudo,
+                descricaoCurta: val.conteudo.substring(0, 100),
+                descricaoMaislidas: val.conteudo.substring(0, 50),
+                imagem: val.imagem,
+                slug: val.slug,
+                categoria: val.categoria,
+                views: val.views,
+              };
+            });
+
+            res.render("home", { posts: posts, postsTop: postsTop });
+          });
+      });
+  } else {
+
+    Posts.find({titulo: {$regex: req.query.busca, $options: "i"}}, function(err, posts){
+        console.log(posts)
+        res.render("busca", {posts: posts, contagem: posts.length});
+    })
+    
+  }
+});
+
+app.get("/:slug", (req, res) => {
+  Posts.findOneAndUpdate(
+    { slug: req.params.slug },
+    { $inc: { views: 1 } },
+    { new: true },
+    function (err, resposta) {
+      if (resposta != null) {
+        Posts.find({})
+          .sort({ views: -1 })
+          .limit(3)
+          .exec(function (err, postsTop) {
+            postsTop = postsTop.map(function (val) {
+              return {
+                titulo: val.titulo,
+                conteudo: val.conteudo,
+                descricaoCurta: val.conteudo.substring(0, 100),
+                descricaoMaislidas: val.conteudo.substring(0, 50),
+                imagem: val.imagem,
+                slug: val.slug,
+                categoria: val.categoria,
+                views: val.views,
+              };
+            });
+
+            res.render("single", { noticia: resposta, postsTop: postsTop });
+          });
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
+
+app.listen(5000, () => {
+  console.log("server rodando!");
+});
